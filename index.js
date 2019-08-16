5fc699379c826ddae4b2954237582c6bd2ec09c1
Berk <berkozturkw@outlook.com>
2019-08-16 18:00:49 +0300

    added default trim function support

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 const trim = (str,char) => {
   str = trimLeft(str,char);
   str = trimRight(str,char);
-  return str;
+  return str.trim();
 }
 const trimLeft = (str,char) => {
   while(str.charAt(0) === char) {
     str = str.substring(1);
   }
-  return str;
+  return str.trimLeft();
 }
 const trimRight = (str,char) => {
     while(str.charAt(str.length-1) === char) {
       str = str.substring(0,str.length-1);
     }
-    return str;
+    return str.trimRight();
 }
 const trimMultiple = (str,...chars) => {
   for(const char of chars){
